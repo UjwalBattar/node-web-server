93aa1a6937dc6005d6ce96468b46a10ce2cf54eb
Ujwal Battar <ujwal.battar@gmail.com>
2018-09-24 19:49:00 -0700

    add projects page

--- server.js
+++ server.js
@@ -53,6 +53,12 @@ app.get('/bad', (request, response) => {
     });
 });
 
+app.get('/projects', (request, response) => {
+    response.render('projects.hbs', {
+        pageTitle: 'Projects Page'
+    });
+});
+
 app.listen(port, () => {
     console.log(`Server is up and running on port ${port}!`);
 
