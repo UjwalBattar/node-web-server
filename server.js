c5b48587abcb0b4c1bac52ef49c3c8e0a518cc65
Ujwal Battar <ujwal.battar@gmail.com>
2018-09-24 18:58:46 -0700

    add express middleware

--- server.js
+++ server.js
@@ -1,10 +1,28 @@
 const express = require('express');
 const hbs = require('hbs');
+const fs = require('fs');
 
 var app = express();
 
 hbs.registerPartials(__dirname + '/views/partials');
 app.set('view engine', 'hbs');
+
+app.use((request, response, next) => {
+    var now = new Date().toString();
+    var log = `${now}: ${request.method} ${request.url}`;
+
+    console.log(log);
+    fs.appendFile('server.log', log + "\n", (err) => {
+        if (err) console.log('Unable to append to server.log.');
+
+    });
+    next();
+});
+
+// app.use((request, response, next) => {
+//     response.render('maintainance.hbs');
+// });
+
 app.use(express.static(__dirname + '/public'));
 
 hbs.registerHelper('getCurrentYear', () => {
