1625ddcb87b9ade642d6eb2bd883daa64b031ade
Ujwal Battar <ujwal.battar@gmail.com>
2018-09-24 17:18:18 -0700

    render templates with data using handlebars

--- server.js
+++ server.js
@@ -1,22 +1,32 @@
 const express = require('express');
+const hbs = require('hbs');
 
 var app = express();
 
+app.set('view engine', 'hbs');
 app.use(express.static(__dirname + '/public'));
 
 app.get('/', (request, response) => {
-    // response.send('<h1>Hello Express!</h1>');
-    response.send({
-        name: 'Ujwal',
-        likes: [
-            'Food',
-            'Music'
-        ]
+    response.render('home.hbs', {
+        pageTitle: 'Home Page',
+        currentYear: new Date().getFullYear(),
+        welcomeMessage: 'Welcome to the Home Page of Some Website'
     });
+    // response.send('<h1>Hello Express!</h1>');
+    // response.send({
+    //     name: 'Ujwal',
+    //     likes: [
+    //         'Food',
+    //         'Music'
+    //     ]
+    // });
 });
 
 app.get('/about', (request, response) => {
-    response.send('About Page');
+    response.render('about.hbs', {
+        pageTitle: 'About Page',
+        currentYear: new Date().getFullYear()
+    });
 });
 
 app.get('/bad', (request, response) => {
